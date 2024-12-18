02295faab022c5d1be26661eeaa926a956b1d8c6
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-12-17 22:08:45 -0300

    Arquivos aula 03 python com banco de dados

--- 20241217-aula03/desafio01.py
+++ 20241217-aula03/desafio01.py
@@ -10,3 +10,56 @@ Desafio Implementar um sistema de leitura e de alarme
 Lista de Bonus
 BONUS: No primeiro item, o valor lido não pode estar fora da faixa entre +5 e menos -5. Exemplo. Se o valor lido anteriormente foi de 60, o novo valor do sensor deve ficar entre 55 e 65. O sensor não deve retornar números negativos, o número mais baixo é 0.
 """
+from random import randint
+from time import sleep
+from typing import List
+
+class Sensor:
+    def __init__(self):
+        self._valor = 0
+
+    def ler(self) -> int:
+        self._valor = randint(0, 100)
+        return self._valor
+    
+class Alarme:
+
+    def soar(self):
+        raise NotImplementedError
+
+class AlarmeSonoro(Alarme):
+
+    def soar(self):
+        print("Alarme sonoro ativo!!!")
+
+
+class AlarmeVisual(Alarme):
+
+    def soar(self):
+        print("Alarme visual ativo!!!")
+
+
+class Cpu:
+
+    def __init__(self, sensor: Sensor, alarmes: List[Alarme]) -> None:
+        self._sensor = sensor
+        self._alarmes = alarmes
+        self._valor_maximo_sensor = 80
+
+    def checar(self) -> None:
+        while True:
+            print("Lendo valor do sensor... ", end="")
+            valor_sensor = self._sensor.ler()
+
+            print(f" {valor_sensor}.")
+
+            if valor_sensor >= 80:
+                for alarme in self._alarmes:
+                    alarme.soar()
+            sleep(1)
+
+if __name__ == "__main__":
+    Cpu(
+        sensor=Sensor(),
+        alarmes=[AlarmeSonoro(), AlarmeVisual()]
+    ).checar()
