8fdae83b0f56728fe4dde71b9d4d53ad25f70b9f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-12-19 20:04:35 -0300

    Arquivos aula 04 python com banco de dados

--- 20241219-aula04/main.py
+++ 20241219-aula04/main.py
@@ -1,6 +1,19 @@
 from config import engine
+from messages import MAIN_MENU
 from models import *
 
 if __name__ == "__main__":
     # O método create_all cria todas as tabelas que ainda não foram criadas no banco de dados
     Base.metadata.create_all(engine)
+
+    while True:
+        print(MAIN_MENU)
+        option = int(input("Informe a opção escolhida: "))
+
+        match option:
+            case 0:
+                print("SAINDO...")
+                break
+
+            case 1:
+                pass
---
+++ 20241219-aula04/messages.py
@@ -0,0 +1,18 @@
+MAIN_MENU = """
+===== ESCOLHA UMA DAS OPÇÕES ABAIXO =====
+==  0 -> SAIR
+==  1 -> GERENCIAR USUÁRIOS
+==  2 -> GERENCIAR CATEGORIAS
+==  3 -> GERENCIAR POSTAGEMS
+==  4 -> GERENCIAR COMENTÁRIOS
+=========================================
+"""
+
+USER_MENU ="""
+===== ESCOLHA UMA DAS OPÇÕES ABAIXO =====
+==  0 -> VOLTAR
+==  1 -> CADASTRAR USUÁRIO
+==  2 -> ALTERAR USUÁRIO
+==  3 -> EXCLUIR USUÁRIO
+=========================================
+"""
